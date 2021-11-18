9718ddb2f84befb92443efa1fade5f2aa1c2421e
mjwawrzyniak <m.j.wawrzyniak@gmail.com>
2021-11-18 14:06:19 -0600

    message

---
+++ GettingStartedWithConditionals/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input;
+input = Console.WriteLine("choose: yYnN");
+if (input == "Y" || input == "y")
+{
+   Console.WriteLine("Yes");    
+} if (input == "N" || input == "n")
+{
+    Console.WriteLine("No")
+}
+
+
+
+
+
+
+
